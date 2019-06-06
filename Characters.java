6ac78b9d65af0e880873b7b26bcee75b76ff3227
Ammar <ammarahmedkhan29@gmail.com>
2019-06-05 22:01:03 -0400

    HLTH variable not working?

--- Characters.java
+++ Characters.java
@@ -3,7 +3,7 @@ public class Characters{
   String name;
   int ATK;
   int DEF;
-  int HLTH;
+  int Health;
   String SP;
   int M1DMG;
 }
@@ -13,17 +13,17 @@ public class Characters{
     public Saiyan(String saiyanSpecialName, String saiyanName){
       ATK = 95;
       DEF = 90;
-      HLTH = 300;
       SP = saiyanSpecialName;
       name = saiyanName;
+      Health = 300;
       }
     }
   class Namekian extends Character{
     public Namekian(String namekianSpecialName, String namekianName){
       ATK = 90;
       DEF = 97;
-      HLTH = 500;
       SP = namekianSpecialName;
       name = namekianName;
+      Health = 500;
     }
   }
